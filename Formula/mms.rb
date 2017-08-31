cd55564d6599dd61dfc858da5330ac31fc23b4ae
XiaQiulei <qiulei.xia@ele.me>
2017-08-31 10:48:44 +0800

    update

--- Formula/mms.rb
+++ Formula/mms.rb
@@ -1,11 +1,10 @@
 class Mms < Formula
-  desc "my mock server"
+  desc "My mock server, mock HTTP server and proxy"
   homepage "https://github.com/LiushuiXiaoxia/MyMockServer"
   url "https://github.com/LiushuiXiaoxia/MyMockServer/raw/master/deploy/mms-0.1.tar"
   sha256 "274e458f2a30e2138a2e621bbb7cab6534d974c4396f404160ff1b7cbda02d6a"
 
   bottle :unneeded
-  
   depends_on :java => "1.6+"
 
   def install
@@ -16,6 +15,6 @@ class Mms < Formula
   end
 
   test do
-
+    File.exist? "#{bin}/mms"
   end
 end
